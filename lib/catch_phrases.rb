db204b70aabbb920ab3f52877685f180a1301047
Lili-Mae Edwards <lilimae@hotmail.co.uk>
2019-12-04 11:22:35 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,19 @@
 def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  phrase = "It's-a me, Mario!"
   puts phrase
 end
 
 def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link
+  phrase = "It's dangerous to go alone! Take this!"
+  puts phrase
+end
+  
+mario 
+toadstool
+link
